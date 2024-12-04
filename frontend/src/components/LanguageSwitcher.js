3734bfee25fe28b6436e434da72b8afe6b3d9198
JustinMa <JustinMa@mail.com>
2024-12-04 15:24:48 -0500

    feat:support more languages

--- frontend/src/components/LanguageSwitcher.js
+++ frontend/src/components/LanguageSwitcher.js
@@ -5,10 +5,10 @@ const LanguageSwitcher = ({className}) => {
   const { i18n } = useTranslation();
 
   const handleLanguageChange = (event) => {
-    const selectedLanguage = event.target.value;
-    i18n.changeLanguage(selectedLanguage);
+    i18n.changeLanguage(event.target.value);
   };
 
+  console.log("i18n.language", i18n.language);
   return (
     <div className={`w-30 ${ className }`} >
       <select
@@ -16,7 +16,13 @@ const LanguageSwitcher = ({className}) => {
         defaultValue={i18n.language}
         className="w-full h-10 px-3 border border-gray-300 rounded-md bg-transparent text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
       >
+        <option value="ar" className="text-black">العربية</option>
+        <option value="de" className="text-black">Deutsch</option>
         <option value="en" className="text-black">English</option>
+        <option value="fr" className="text-black">Français</option>
+        <option value="ja" className="text-black">日本語</option>
+        <option value="ko" className="text-black">한국어</option>
+        <option value="ru" className="text-black">Русский</option>
         <option value="zh" className="text-black">中文</option>
       </select>
     </div>
--- frontend/src/i18n/i18n.js
+++ frontend/src/i18n/i18n.js
@@ -2,7 +2,14 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
+
+import ar from './locales/ar.json';
+import de from './locales/de.json';
 import en from './locales/en.json';
+import fr from './locales/fr.json';
+import ja from './locales/ja.json';
+import ko from './locales/ko.json';
+import ru from './locales/ru.json';
 import zh from './locales/zh.json';
 
 const detectBrowserLanguage = () => {
@@ -22,7 +29,13 @@ i18n
       escapeValue: false,
     },
     resources: {
+      ar: { translation: ar },
+      de: { translation: de },
       en: { translation: en },
+      fr: { translation: fr },
+      ja: { translation: ja },
+      ko: { translation: ko },
+      ru: { translation: ru },
       zh: { translation: zh },
     },
   });
